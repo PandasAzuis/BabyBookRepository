5fa0843a29b5265d26e95f8dc8f59b92e669797d
Nicoly Melo <nicoly.melo18@outlook.com>
2020-06-25 18:20:32 -0300

    SplashPage was created

---
+++ BabyBook/BabyBook/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BabyBook.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BabyBook
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new SplashPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BabyBook/BabyBook/View/SplashPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BabyBook.View
+{
+    public class SplashPage : ContentPage
+    {
+        Image splashImage;
+
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var sub = new AbsoluteLayout();
+
+            // Image to be loaded
+            splashImage = new Image
+            {
+                Source = "BabyShadowGray.png",
+                WidthRequest = 350,
+                HeightRequest = 350
+            };
+            AbsoluteLayout.SetLayoutFlags(splashImage, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage, new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            sub.Children.Add(splashImage);
+
+            this.BackgroundColor = Color.FromHex("#FFB6C1");
+            this.Content = sub;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Animation effect
+            await splashImage.ScaleTo(1, 2000);
+            await splashImage.ScaleTo(0.8, 1400, Easing.Linear);
+            await splashImage.ScaleTo(140, 1100, Easing.Linear);
+
+            Application.Current.MainPage = new MainPage();
+        }
+
+    }
+}
